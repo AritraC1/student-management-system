a68cec13c14bc4e20a36973d141ca460caf9d740
Aritra Chatterjee <ach142812@gmail.com>
2026-09-03 10:49:07 +0530

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include "app/app.hpp"
 
 int main() {
+    // Log
     Logger::init("logs/log-file.txt");
     Logger::log(LogLevel::INFO, "Application started");
 
+    // Main app run
     Application app;
     app.run();
 
